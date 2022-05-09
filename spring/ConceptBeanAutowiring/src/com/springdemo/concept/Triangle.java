930286c704cf7be56dc7dfc5d17995e493b87669
1snehal <78212185+1snehal@users.noreply.github.com>
2022-05-09 09:06:56 +0530

    Add files via upload

---
+++ spring/ConceptBeanAutowiring/src/com/springdemo/concept/Triangle.java
@@ -0,0 +1,47 @@
+package com.springdemo.concept;
+
+import java.util.List;
+
+public class Triangle {
+	private Point pointa;
+	private Point pointb;
+	private Point pointc;
+
+
+	public Point getPointa() {
+		return pointa;
+	}
+
+
+	public void setPointa(Point pointa) {
+		this.pointa = pointa;
+	}
+
+
+	public Point getPointb() {
+		return pointb;
+	}
+
+
+	public void setPointb(Point pointb) {
+		this.pointb = pointb;
+	}
+
+
+	public Point getPointc() {
+		return pointc;
+	}
+
+
+	public void setPointc(Point pointc) {
+		this.pointc = pointc;
+	}
+
+
+	public void sketch() {
+		System.out.println("Point A =...."+getPointa().getX()+","+getPointa().getY());
+		System.out.println("Point B =...."+getPointb().getX()+","+getPointb().getY());
+		System.out.println("Point C =...."+getPointc().getX()+","+getPointc().getY());
+	}
+
+}
